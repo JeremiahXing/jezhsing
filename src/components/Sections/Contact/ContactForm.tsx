508e07d00f52796ea1200606dca5c6b8a2a7b623
Jeremiah Hsing <jeremiah.hsing@gmail.com>
2023-02-02 01:42:44 +1100

    fix unknown issues

--- src/components/Sections/Contact/ContactForm.tsx
+++ src/components/Sections/Contact/ContactForm.tsx
@@ -49,18 +49,16 @@ const ContactForm: FC = memo(() => {
         const vaildationData = await vaildationResponse.json();
         console.log(vaildationData);
         if (vaildationData.is_smtp_valid.value) {
-          // setIsSending(true);
           const serviceID: string = process.env.EMAILJS_SERVICE_ID ? process.env.EMAILJS_SERVICE_ID : '';
           const templateID: string = process.env.EMAILJS_TEMPLATE_ID ? process.env.EMAILJS_TEMPLATE_ID : '';
           const publicKey: string = process.env.EMAILJS_PUBLIC_KEY ? process.env.EMAILJS_PUBLIC_KEY : '';
           emailjs.sendForm(serviceID, templateID, event.target as HTMLFormElement, publicKey)
-            .then(result => {
+            .then((result) => {
               alert('Thanks for reaching out! We will get back to you shortly.');
               console.log(result.text);
-            }) 
-            .catch(error => {
+            }, (error) => {
               alert('Something went wrong. Please try again later.');
-              console.error(error);
+              console.error(error.text);
             });
         } else {
           alert('Please enter a valid email address');
